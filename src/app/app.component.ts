fa139a764830fbd31317628d0927e81e551b3f2a
hagerCh <hagerchaabane1@gmail.com>
2019-04-03 19:17:52 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app2';
+  
+  val='';
+  number='';
+  operation='';
+
+  numberClick(op){
+    if(op==''){
+      this.operation='';
+      this.val='';
+    }
+
+    if (op in ['0','1','2','3','4','5','6','7','8','9']){
+      this.number=this.number + op;
+      this.val=this.number;
+      //console.log("number:"+this.number);
+    }
+    else
+      {
+       this.number='';
+        this.val= op;
+      }
+    this.operation=this.operation+op;
+    //console.log(this.operation);
+  }
+  result(){
+    this.val= eval(this.operation);
+    this.operation='';
+    this.number='';
+  }
 }
